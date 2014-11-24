392f636582d9c92a11810cf0d731646afb09d962
Sean Jewell <jewellsean@gmail.com>
2014-11-24 10:50:25 -0800

    edit method...not currently implemented, deprecate CNCopyNumberMixture

--- src/main/java/conifer/models/CNMultiCategorySubstitutionModel.java
+++ src/main/java/conifer/models/CNMultiCategorySubstitutionModel.java
@@ -1,7 +1,11 @@
 package conifer.models;
 
+import org.ejml.simple.SimpleMatrix;
+
 import blang.annotations.FactorArgument;
 import conifer.Parsimony;
+import conifer.TreeNode;
+import conifer.ctmc.CTMC;
 
 /**
  * 
@@ -9,17 +13,36 @@ import conifer.Parsimony;
  *
  * @param <T>
  */
+
 @Deprecated
 public class CNMultiCategorySubstitutionModel<T extends RateMatrixMixture> extends
         MultiCategorySubstitutionModel<T>
 {
 
-    public CNMultiCategorySubstitutionModel(T rateMatrixMixture, Parsimony parsimony, int nSites)
+    public CNMultiCategorySubstitutionModel(T rateMatrixMixture, int nSites)
     {
-        super(rateMatrixMixture, nSites);
-        this.parsimony = parsimony; 
+        super(rateMatrixMixture, nSites); 
     }
 
-    public final Parsimony parsimony; 
+    // Specify normal rooting
+    @Override
+    public void buildInitialDistribution(TreeNode node,
+        LikelihoodFactoryContext context)
+    {
+      if (node.toString() != "root")
+          throw new RuntimeException("Initial distribution must be built on the root node!");
+        
+      final int categoryIndex = context.getFactorGraphIndex();
+      double[][] ctmc = rateMatrixMixture.getRateMatrix(categoryIndex).getRateMatrix();
+      int nStates = ctmc[0].length; 
+      double [] stationary = new double[nStates];
+      stationary[2] = 1; 
+      double [][] allStatios = new double[nSites][stationary.length];
+      for (int siteIndex = 0; siteIndex < nSites; siteIndex++)
+        allStatios[siteIndex] = stationary;
+      context.getDiscreteFactorGraph().unaryTimesEqual(node, new SimpleMatrix(allStatios));
+    }
+    
+    
     
 }
