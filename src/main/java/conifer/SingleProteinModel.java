26b2e899ac2d35d1a58c61854a5a1f1b751dc56a
Tingting <zhaott0416@gmail.com>
2015-06-25 17:36:01 -0700

    add the value of useAuxiliaryVariable (true/false) in the name of the result folder

--- src/main/java/conifer/SingleProteinModel.java
+++ src/main/java/conifer/SingleProteinModel.java
@@ -184,7 +184,7 @@ public class SingleProteinModel implements Runnable, Processor
         logToFile("Total time in minutes: " + ((System.currentTimeMillis() - startTime)/60000.0));
         //File newDirectory = new File(Results.getResultFolder().getParent() + "rep"+ rep+ "isExcludedHMCMove" + isExcluded + bandwidth+selectedRateMtx+"numSites"+numberOfSites+"Seed"+whichSeedUsed+ "epsilon"+PhyloHMCMove.epsilon+"L"+PhyloHMCMove.L);
         File newDirectory = new File(Results.getResultFolder().getParent() + "rep"+ rep+ "isExcludedHMCMove" + isExcluded + "slice"+sliceSampler+bandwidth+selectedRateMtx+"numSites"+numberOfSites+"Seed"+whichSeedUsed +"epsilon"+PhyloHMCMove.epsilon+"L"+PhyloHMCMove.L+"Adapt"+ PhyloHMCMove.sizeAdapt+
-                "nItersPerPathAuxVar"+nItersPerPathAuxVar);
+                "nItersPerPathAuxVar"+nItersPerPathAuxVar+"useAux"+ useAuxiliaryVariable);
         newDirectory.mkdir();
         try
         {
