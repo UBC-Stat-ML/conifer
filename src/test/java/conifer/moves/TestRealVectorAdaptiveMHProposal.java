10ba8aa8fe6b093e64a019cb7e7a6e20d60f66de
Tingting <zhaott0416@gmail.com>
2015-07-05 00:14:07 -0700

    commit TestRealVectorAdaptiveMHProposal

--- src/test/java/conifer/moves/TestRealVectorAdaptiveMHProposal.java
+++ src/test/java/conifer/moves/TestRealVectorAdaptiveMHProposal.java
@@ -4,24 +4,24 @@ import bayonet.distributions.Normal;
 import bayonet.math.EJMLUtils;
 import blang.annotations.DefineFactor;
 import blang.factors.IIDRealVectorGenerativeFactor;
+import briefj.BriefIO;
 import briefj.run.Results;
 import com.beust.jcommander.internal.Lists;
 import conifer.ctmc.PathStatistics;
 import conifer.ctmc.expfam.*;
 import conifer.factors.UnrootedTreeLikelihood;
 import conifer.models.MultiCategorySubstitutionModel;
+import org.ejml.data.DenseMatrix64F;
+import org.ejml.ops.RandomMatrices;
 import org.ejml.simple.SimpleMatrix;
 import org.jblas.DoubleMatrix;
 import org.junit.Test;
-
 
 import java.io.File;
 import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
-
-import briefj.BriefIO;
 
 /**
  * Test the agreement between pure forward sampler vs.
@@ -32,110 +32,91 @@ import briefj.BriefIO;
  */
 public class TestRealVectorAdaptiveMHProposal
 {
-
-
-    private final PrintWriter detailWriter = BriefIO.output(Results.getFileInResultFolder("experiment.details.txt"));
-
-    public static void main(String [] args)
-    {
-        new TestRealVectorAdaptiveMHProposal().test();
-    }
-
-    private final double Threshold = 0.5;
-
     @Test
     public void test()
     {
-        File inputFile = new File("/Users/crystal/IdeaProjects/conifer/src/main/resources/conifer/sampleInput/FES_4.fasta");
-        File treeFile = new File("/Users/crystal/IdeaProjects/conifer/src/main/resources/conifer/sampleInput/FES.ape.4.nwk");
-        RateMtxNames selectedRateMtx = RateMtxNames.DNAGTR;
-        CTMCExpFam<CTMCState>.ExpectedReversibleObjectiveUpdateExpectedStat noAuxObjective =null;
-        class Model {
+        int dim =8;
+        // generate a matrix of 100* 8, 100 observations of dimension 8;
+        // Test generateMultiNormal and updateMeanAndCov method of this class
 
-            @DefineFactor(onObservations = true)
-            public final UnrootedTreeLikelihood<MultiCategorySubstitutionModel<ExpFamMixture>> likelihood1 =
-                    UnrootedTreeLikelihood
-                            .fromFastaFile(inputFile, selectedRateMtx)
-                            .withExpFamMixture(ExpFamMixture.rateMtxModel(selectedRateMtx))
-                            .withTree(treeFile);
+        //Test generateMultiNormal method, generate data first under an arbitrary covariance matrix
+        Random rand = new Random(1);
+        double [][] cov = new double[dim][dim];
+        DenseMatrix64F covariance = RandomMatrices.createSymmPosDef(dim, rand);
+        for(int i=0; i<dim; i++){
+            for(int j=0; j< dim; j++){
+                cov[i][j] = covariance.get(i, j);
 
-            @DefineFactor
-            public final IIDRealVectorGenerativeFactor<Normal.MeanVarianceParameterization> prior =
-                    IIDRealVectorGenerativeFactor
-                            .iidNormalOn(likelihood1.evolutionaryModel.rateMatrixMixture.parameters);
+            }
+        }
+
+        double [] mean = new double[dim];
+        for(int i=0; i< dim; i++){
+            mean[i] = rand.nextGaussian();
+        }
+        System.out.println(Arrays.toString(mean));
+
+        int nSamples = 50000;
+        double [][] samples = new double [nSamples][dim];
+
+        for(int j=0; j< nSamples; j++){
+
+            samples[j] = RealVectorAdaptiveMHProposal.generateMultiNormal(rand, mean, cov, 1);
 
         }
 
-        Model model = new Model();
+        // obtain sample mean estimate
+        double [] meanEst = new double [dim];
+        meanEst = new DoubleMatrix(samples).columnMeans().toArray();
+        System.out.println(Arrays.toString(meanEst));
 
-        UnrootedTreeLikelihood<MultiCategorySubstitutionModel<ExpFamMixture>> likelihood = model.likelihood1;
-        ExpFamParameters parameters = likelihood.evolutionaryModel.rateMatrixMixture.parameters;
+        // test if the true mean and sample mean are close enough
+        EJMLUtils.checkIsClose(new SimpleMatrix(new DoubleMatrix(mean).toArray2()), new SimpleMatrix(new DoubleMatrix(meanEst).toArray2()), 0.05);
 
-
-        int numOfWeights = 5;
-        for(int j=0; j< numOfWeights; j++){
-            double [] newWeight = new double [parameters.getVector().length];
-            for(int i=0; i< parameters.getVector().length;i++){
-                newWeight[i] = new Random(i+j).nextGaussian();
-            }
-
-            parameters.setVector(newWeight);
-
-            System.out.println(Arrays.toString(likelihood.evolutionaryModel.rateMatrixMixture.parameters.getVector()));
-
-            ExpectedStatistics<CTMCState> expectedStatistics = likelihood.evolutionaryModel.getTotalExpectedStatistics(likelihood.observations, likelihood.tree, parameters.globalExponentialFamily);
-            noAuxObjective = parameters.globalExponentialFamily.getExpectedReversibleObjectiveUpdateExpectedStat(1.0, likelihood.observations,
-                    likelihood.tree, likelihood.evolutionaryModel, expectedStatistics, parameters);
-            double [] gradientFromExpectedStat = noAuxObjective.derivativeAt(parameters.getVector());
-
-            // next I get the averaged gradient using auxiliary variables and average the gradients obtained from sampled values of the sufficient statistics
-            List<List<PathStatistics>> allPathStatistics = Lists.newArrayList();
-            List<ExpectedStatistics<CTMCState>> convertedStatLists = Lists.newArrayList();
-            List<CTMCExpFam<CTMCState>.ExpectedCompleteReversibleObjective> auxObjectiveLists = Lists.newArrayList();
-
-            int nRep =5000;
-            DoubleMatrix allInit = new DoubleMatrix(1, likelihood.evolutionaryModel.rateMatrixMixture.getRateMatrix(0).getRateMatrix().length);
-            DoubleMatrix allHoldingtime = new DoubleMatrix(1, likelihood.evolutionaryModel.rateMatrixMixture.getRateMatrix(0).getRateMatrix().length);
-            DoubleMatrix allNTrans = new DoubleMatrix(likelihood.evolutionaryModel.rateMatrixMixture.getRateMatrix(0).getRateMatrix().length,
-                    (likelihood.evolutionaryModel.rateMatrixMixture.getRateMatrix(0).getRateMatrix().length-1));
-
-
-            for(int i=0; i< nRep; i++){
-                allPathStatistics.add(i, likelihood.evolutionaryModel.samplePosteriorPaths(new Random(i), likelihood.observations, likelihood.tree));
-                convertedStatLists.add(i, PhyloHMCMove.convert(allPathStatistics.get(i), parameters, likelihood));
-                // sum over all the sufficient statistics across all iterations and outside this for loop, we will get an average value
-                allInit.addi(new DoubleMatrix(convertedStatLists.get(i).nInit).transpose());
-                allHoldingtime.addi(new DoubleMatrix((convertedStatLists.get(i).holdTimes)).transpose());
-                allNTrans.addi(new DoubleMatrix(convertedStatLists.get(i).nTrans));
-                //auxObjectiveLists.add(i, parameters.globalExponentialFamily.getExpectedCompleteReversibleObjective(1.0, convertedStatLists.get(i)));
-            }
-
-            // get averaged sufficient statistics of NInit, holdingTime and nTrans across all nRep iterations
-            double [] avgInit = allInit.div(nRep).toArray();
-            double [] avgHoldingTime = allHoldingtime.div(nRep).toArray();
-            double [][] avgNTrans = allNTrans.div(nRep).toArray2();
-
-            ExpectedStatistics<CTMCState> avgExpectedStat = convertedStatLists.get(0);
-            avgExpectedStat.nInit = avgInit;
-            avgExpectedStat.holdTimes=avgHoldingTime;
-            avgExpectedStat.nTrans=avgNTrans;
-
-            CTMCExpFam<CTMCState>.ExpectedCompleteReversibleObjective auxObjective =  parameters.globalExponentialFamily.getExpectedCompleteReversibleObjective(1.0, avgExpectedStat);
-            double [] avgGradient = auxObjective.derivativeAt(parameters.getVector());
-            System.out.println(Arrays.toString(avgGradient));
-
-//            double [][] allGradient = new double [nRep][parameters.getVector().length];
-//            for(int i=0; i< nRep; i++){
-//                allGradient[i] = auxObjectiveLists.get(i).derivativeAt(parameters.getVector());
-//            }
-//            DoubleMatrix allGradientMtx = new DoubleMatrix(allGradient);
-//            double []  averagedGradient = allGradientMtx.columnMeans().toArray();
-//            System.out.println(Arrays.toString(averagedGradient));
-            System.out.println(Arrays.toString(gradientFromExpectedStat));
-//            EJMLUtils.checkIsClose(new SimpleMatrix(allGradientMtx.columnMeans().transpose().toArray2()), new SimpleMatrix(new DoubleMatrix(gradientFromExpectedStat).toArray2()), 0.5);
-            EJMLUtils.checkIsClose(new SimpleMatrix(new DoubleMatrix(avgGradient).toArray2()), new SimpleMatrix(new DoubleMatrix(gradientFromExpectedStat).toArray2()), Threshold);
-            System.out.println(j);
+        // calculate the sample covariance matrix to see if it is close to the true covariance matrix cov
+        double [][] sampleCov = new double [dim][dim];
+        DoubleMatrix sampleCovMtx = new DoubleMatrix(sampleCov);
+        DoubleMatrix sampleMtx = new DoubleMatrix(samples);
+        DoubleMatrix meanEstMtx = new DoubleMatrix(meanEst);
+        DoubleMatrix tmp;
+        DoubleMatrix tmpCovEachObs;
+        for(int i =0; i< nSamples; i++){
+            tmp = sampleMtx.getRow(i).sub(meanEstMtx);
+            tmpCovEachObs = tmp.transpose().mmul(tmp).div(nSamples);
+            sampleCovMtx = sampleCovMtx.add(tmpCovEachObs);
         }
+
+
+        sampleCov = sampleCovMtx.toArray2();
+        EJMLUtils.checkIsClose(new SimpleMatrix(cov), new SimpleMatrix(sampleCov), 0.02);
+
+        System.out.println(Arrays.deepToString(cov));
+        System.out.println(Arrays.deepToString(sampleCov));
+
+
+        // check the updateMeanAndCov() method, this is to check whether the sample mean and sample covariance matrix are the same
+        // either we calculate them directly or with the recursion formula used in method RealVectorAdpativeMHProposal.updateMeanAndCov()
+        RealVectorAdaptiveMHProposal.meanVector = samples[0];
+        RealVectorAdaptiveMHProposal.sampleCov = DoubleMatrix.eye(dim).toArray2();
+
+        for(int j= 1; j< nSamples; j++){
+
+            RealVectorAdaptiveMHProposal.updateMeanAndCov(j, samples[j]);
+        }
+
+        System.out.println(Arrays.deepToString(RealVectorAdaptiveMHProposal.sampleCov));
+
+        EJMLUtils.checkIsClose(new SimpleMatrix(sampleCov), new SimpleMatrix(RealVectorAdaptiveMHProposal.sampleCov), 0.001);
+
+        double [][] meanEst2d = new double [1][meanEst.length];
+        meanEst2d[0] = meanEst;
+        double [][] meanVector = new double [1][RealVectorAdaptiveMHProposal.meanVector.length];
+        meanVector[0] = RealVectorAdaptiveMHProposal.meanVector;
+
+        System.out.println(Arrays.toString(meanEst));
+        System.out.println(Arrays.toString(RealVectorAdaptiveMHProposal.meanVector));
+
+        EJMLUtils.checkIsClose(new SimpleMatrix(meanEst2d), new SimpleMatrix(meanVector), 0.001);
 
 
 
