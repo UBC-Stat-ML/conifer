cc9bd95c2e99cbd9ffacb0886939e60e4051f506
Alexandre Bouchard <alexandre.bouchard@gmail.com>
2017-09-21 12:17:58 -0700

    NNI almost there

--- src/main/java/conifer/moves/SingleNNI.java
+++ src/main/java/conifer/moves/SingleNNI.java
@@ -5,10 +5,9 @@ import java.util.Random;
 
 import org.jgrapht.Graphs;
 
-import bayonet.distributions.DiscreteUniform;
-import blang.factors.Factor;
+import blang.mcmc.Callback;
 import blang.mcmc.ConnectedFactor;
-import blang.mcmc.MHProposalDistribution;
+import blang.mcmc.MHSampler;
 import blang.mcmc.SampledVariable;
 import briefj.collections.UnorderedPair;
 
@@ -20,24 +19,22 @@ import conifer.UnrootedTree;
 
 
 
-public class SingleNNI implements MHProposalDistribution
+public class SingleNNI extends MHSampler<UnrootedTree>
 {
-  @SampledVariable UnrootedTree tree;
-  
-  @ConnectedFactor List<Factor> factors;
-
-
   @Override
-  public Proposal propose(Random rand)
+  public void propose(Random rand, Callback callback)
   {
-    List<UnorderedPair<TreeNode, TreeNode>> nonTerminalEdges = TopologyUtils.nonTerminalEdges(tree.getTopology());
+    List<UnorderedPair<TreeNode, TreeNode>> nonTerminalEdges = TopologyUtils.nonTerminalEdges(variable.getTopology());
     if (nonTerminalEdges.isEmpty())
-      return null; // If its just a pair of sequence, we do not need to propose anything.
+      return;
+    callback.setProposalLogRatio(0.0);
     UnorderedPair<TreeNode, TreeNode> referenceEdge = DiscreteUniform.sample(nonTerminalEdges, rand);
     TreeNode moved1 = sampleMovedEndpoint(referenceEdge, referenceEdge.getFirst(), rand);
     TreeNode moved2 = sampleMovedEndpoint(referenceEdge, referenceEdge.getSecond(), rand);
-    tree.interchange(moved1, referenceEdge.getFirst(), moved2, referenceEdge.getSecond());
-    return new ProposalRealization(moved2, referenceEdge.getFirst(), moved1, referenceEdge.getSecond());
+    variable.interchange(moved1, referenceEdge.getFirst(), moved2, referenceEdge.getSecond());
+    if (!callback.sampleAcceptance())
+      variable.interchange(moved1, referenceEdge.getFirst(), moved2, referenceEdge.getSecond());
+
   }
   
   private TreeNode sampleMovedEndpoint(
@@ -46,7 +43,7 @@ public class SingleNNI implements MHProposalDistribution
       Random rand)
   {
     List<TreeNode> choices = Lists.newArrayList();
-    for (TreeNode neibhbor : Graphs.neighborListOf(tree.getTopology(), fixedEndPoint))
+    for (TreeNode neibhbor : Graphs.neighborListOf(variable.getTopology(), fixedEndPoint))
       if (!neibhbor.equals(referenceEdge.getFirst()) &&
           !neibhbor.equals(referenceEdge.getSecond()))
         choices.add(neibhbor);
@@ -54,34 +51,4 @@ public class SingleNNI implements MHProposalDistribution
       throw new RuntimeException();
     return DiscreteUniform.sample(choices, rand);
   }
-  
-  private class ProposalRealization implements Proposal
-  {
-    private final TreeNode moved1, fixed1, moved2, fixed2;
-    private boolean done = false;
-    
-    private ProposalRealization(TreeNode moved1, TreeNode fixed1,
-        TreeNode moved2, TreeNode fixed2)
-    {
-      this.moved1 = moved1;
-      this.fixed1 = fixed1;
-      this.moved2 = moved2;
-      this.fixed2 = fixed2;
-    }
-    @Override
-    public double logProposalRatio()
-    {
-      return 0;
-    }
-    @Override
-    public void acceptReject(boolean accept)
-    {
-      if (done)
-        throw new RuntimeException();
-      done = true;
-      if (!accept)
-        tree.interchange(moved1, fixed1, moved2, fixed2);
-    }
-  }
-
 }
